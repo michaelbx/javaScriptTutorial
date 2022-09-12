f20eec3053ab3a8166b23d14c2f41b0447b82e5e
unknown <mihai10rusu@gmail.com>
2022-09-12 22:11:27 +0300

    arrays /vectori

--- probleme test/main.js
+++ probleme test/main.js
@@ -1,8 +1,23 @@
-let x=2;
-const y=4;
-console.log("value of variable x is = "+x);
-console.log("value of constant y is ="+y);
-x=x+2;
-y=y+2;
-console.log("value of variable x is = "+x);
-console.log("value of constant y is ="+y);
+
+let array=[];
+array.push(1);
+array.push("pula");
+console.log("lungimea vectorului  este de = "+array.length);
+console.log("vectorului la poz 0 = "+array[0]);
+console.log("vectorului la poz 1 = "+array[1]);
+array=array.concat([1,2,3,45,5,7,8,10,12,12.3455,"final"]);
+console.log("lungimea vectorului  este de = "+array.length);
+
+// for(let i=0;i<array.length;i++){
+//     console.log("Array at pozition: "+i+" has the value "+array[i]);
+// }
+
+// array.map((elem) => console.log("Array has the value"+elem ));
+// array.map((elem,i) => console.log("Array at pozition: "+i+" has the value "+elem));
+let array2=[1,2,3,4,45,67,90];
+array2.map((elem,i) => console.log("Array at pozition: "+i+" has the value "+elem));
+
+//punem intrun array toate valorile din array2 mai mari ca 10
+
+let result=array2.filter((elem)=> elem>10);
+result.map((elem,i) => console.log("Array at pozition: "+i+" has the value "+elem));
