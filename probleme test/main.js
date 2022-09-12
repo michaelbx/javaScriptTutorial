3a603cb9386449931c633fc733ce239763e1230c
unknown <mihai10rusu@gmail.com>
2022-09-12 21:45:53 +0300

    calculus

---
+++ probleme test/main.js
@@ -0,0 +1,8 @@
+let x=2;
+const y=4;
+console.log("value of variable x is = "+x);
+console.log("value of constant y is ="+y);
+x=x+2;
+y=y+2;
+console.log("value of variable x is = "+x);
+console.log("value of constant y is ="+y);
